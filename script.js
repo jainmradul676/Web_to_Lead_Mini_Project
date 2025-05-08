80f76ffa6811e486a085f16be69a7e5952ef921d
mj876 <mradul.jain@hexaviewtech.com>
2025-05-08 16:58:20 +0530

    recaptcha validation and styles changes

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function beforeSubmit() {
-  let outputDate = document.querySelector(".outputDate");
-  let inputDate = document.querySelector(".inputDate");
-  //   console.log(inputDate);
-  let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
-  outputDate.value = formattedDate;
+let captchaChecked = false;
+function beforeSubmit(event) {
+  if (captchaChecked) {
+    let outputDate = document.querySelector(".outputDate");
+    let inputDate = document.querySelector(".inputDate");
+    //   console.log(inputDate);
+    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
+    outputDate.value = formattedDate;
+  } else {
+    alert("Please check the captcha first, to sybmit the form.");
+    event.preventDefault(); // Prevent form submission
+  }
 }
 
 function timestamp() {
@@ -18,3 +24,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchaSuccess() {
+  captchaChecked = true;
+}
